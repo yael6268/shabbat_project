f6e258d6760035ee8499c20a64a381c3fb3d72d0
yael6268 <yaelk6268@gmail.com>
2025-12-31 00:32:28 +0200

    add shopingList

--- shabbat/src/main.jsx
+++ shabbat/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Home } from './components/home.jsx'
+// import { Shoping } from './components/shoping.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Home/>
+    <Home/>  
+    {/* <Shoping/> */}
   </StrictMode>,
+
 )
